de8fd5461532354a2b8ab6965e308dd571e81996
UtBots-Home <utbots.home@gmail.com>
2025-05-06 14:19:37 -0300

    One command mapping done

---
+++ utbots_nav/launch/map.launch.py
@@ -0,0 +1,39 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+'''
+export BASE_MODEL=hestia
+'''
+
+def generate_launch_description():
+
+    utbots_nav_launch_file_dir = os.path.join(get_package_share_directory('utbots_nav'), 'launch')
+    rplidar_launch_file_dir = os.path.join(get_package_share_directory('rplidar_ros'), 'launch')
+
+    return LaunchDescription([
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/hoverboard.launch.py']),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/utbots_mapping.launch.py']),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/rplidar.launch.py']),
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+        ),
+
+        ])
