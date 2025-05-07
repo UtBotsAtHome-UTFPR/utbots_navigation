6e0070d522c9e7f416ffe0a76c2f3437de67e4fc
gustavo-fardo <gustavo.fardo@hotmail.com>
2025-05-06 22:57:57 -0300

    box filter added and refactor in the launchfiles, not added map arg yet in nav.yaml

--- utbots_nav/launch/map.launch.py
+++ utbots_nav/launch/map.launch.py
@@ -14,26 +14,52 @@ export BASE_MODEL=hestia
 '''
 
 def generate_launch_description():
+    # Arguments
+    input_scan = LaunchConfiguration('input_scan_topic')
+    output_scan = LaunchConfiguration('filtered_scan_topic')
 
     utbots_nav_launch_file_dir = os.path.join(get_package_share_directory('utbots_nav'), 'launch')
-    rplidar_launch_file_dir = os.path.join(get_package_share_directory('rplidar_ros'), 'launch')
 
     return LaunchDescription([
-
+        # Launch Arguments
+        DeclareLaunchArgument(
+            'input_scan_topic',
+            default_value='scan',
+            description='Input LaserScan topic name'
+        ),
+        DeclareLaunchArgument(
+            'filtered_scan_topic',
+            default_value='scan_filtered',
+            description='Output (filtered) LaserScan topic name'
+        ),
+        # Hoverboard Diffbot Driver
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/hoverboard.launch.py']),
         ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/utbots_mapping.launch.py']),
-        ),
+        # LIDAR Driver
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/rplidar.launch.py']),
         ),
+        # Mapping launchfile
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/utbots_mapping.launch.py']),
+        ),
+        # Laser Filter
+        Node(
+            package='laser_filters',
+            executable='scan_to_scan_filter_chain',
+            name='laser_filter',
+            parameters=['box_filter.yaml'],
+            remappings=[
+                ('scan', input_scan),
+                ('scan_filtered', output_scan)
+            ]
+        ),
+        # RVIZ2
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             output='screen',
-        ),
-
+        )
         ])
--- utbots_nav/launch/nav.launch.py
+++ utbots_nav/launch/nav.launch.py
@@ -17,20 +17,52 @@ TODO: adjust which parameters should be available to chenge configs on the 3 pul
 '''
 
 def generate_launch_description():
+    # Arguments
+    input_scan = LaunchConfiguration('input_scan_topic')
+    output_scan = LaunchConfiguration('filtered_scan_topic')
+    map_dir = LaunchConfiguration('map')
 
     utbots_nav_launch_file_dir = os.path.join(get_package_share_directory('utbots_nav'), 'launch')
-    rplidar_launch_file_dir = os.path.join(get_package_share_directory('rplidar_ros'), 'launch')
 
     return LaunchDescription([
+        # Launch Arguments
+        DeclareLaunchArgument(
+            'input_scan_topic',
+            default_value='scan',
+            description='Input LaserScan topic name'
+        ),
+        DeclareLaunchArgument(
+            'filtered_scan_topic',
+            default_value='scan_filtered',
+            description='Output (filtered) LaserScan topic name'
+        ),
+        # Load Map
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
 
+        # Hoverboard Diffbot Driver
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/hoverboard.launch.py']),
         ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/utbots_navigation.launch.py']),
-        ),
+        # LIDAR Driver
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/rplidar.launch.py']),
         ),
-
+        # Mapping launchfile
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/utbots_navigation.launch.py']),
+        ),
+        # Laser Filter
+        Node(
+            package='laser_filters',
+            executable='scan_to_scan_filter_chain',
+            name='laser_filter',
+            parameters=['box_filter.yaml'],
+            remappings=[
+                ('scan', input_scan),
+                ('scan_filtered', output_scan)
+            ]
+        )
         ])
