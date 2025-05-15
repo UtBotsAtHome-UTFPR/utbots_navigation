fc30eb36578a59247f27bd12d0e37490baf7cab4
Gustavo Fardo Armênio <78488285+gustavo-fardo@users.noreply.github.com>
2025-05-15 15:19:53 -0300

    update nav.launch.py arguments

--- utbots_nav/launch/nav.launch.py
+++ utbots_nav/launch/nav.launch.py
@@ -12,26 +12,23 @@ from launch.substitutions import PathJoinSubstitution
 
 '''
 export BASE_MODEL=hestia
-
-TODO: set this as default parameter
-TODO: adjust which parameters should be available to chenge configs on the 3 pulled launch files
 '''
 
 def generate_launch_description():
     # Arguments
-    input_scan = LaunchConfiguration('input_scan_topic')
-    output_scan = LaunchConfiguration('filtered_scan_topic')
-    map_dir = LaunchConfiguration(
-            'map',
-            default=os.path.join(
-                get_package_share_directory('utbots_nav'),
-                'map',
-                'corredor2.yaml'))
+    input_scan_topic = LaunchConfiguration('input_scan_topic')
+    filtered_scan_topic = LaunchConfiguration('filtered_scan_topic')
+    map_dir = LaunchConfiguration('map')
+    use_imu = LaunchConfiguration('use_imu')
+    use_rviz = LaunchConfiguration('use_rviz')
+    lidar_port = LaunchConfiguration('lidar_port')
 
     utbots_nav_launch_file_dir = os.path.join(get_package_share_directory('utbots_nav'), 'launch')
 
     return LaunchDescription([
         # Launch Arguments
+        
+        # Scan topics
         DeclareLaunchArgument(
             'input_scan_topic',
             default_value='scan',
@@ -45,21 +42,50 @@ def generate_launch_description():
         # Load Map
         DeclareLaunchArgument(
             'map',
-            default_value=map_dir,
+            default_value=os.path.join(
+                get_package_share_directory('utbots_nav'),
+                'map',
+                'corredor2.yaml'),
             description='Full path to map file to load'),
+
+         # Use IMU
+        DeclareLaunchArgument(
+            'use_imu',
+            default_value='true',
+            description='Set to true to launch robot_localization'
+        ),
+
+        # Launch with RVIZ
+        DeclareLaunchArgument(
+            'use_rviz',
+            default_value='true',
+            description='Use RVIZ2 for visualization'
+        ),
+
+        # Launch with RVIZ
+        DeclareLaunchArgument(
+            'lidar_port',
+            default_value='/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+            description='Define LIDAR serial port'
+        ),
 
         # Hoverboard Diffbot Driver
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/hoverboard.launch.py']),
         ),
+        
         # LIDAR Driver
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/rplidar.launch.py']),
+            launch_arguments={'lidar_port': lidar_port}.items()  # Pass map as an argument to the mapping launchfile
         ),
+        
         # Mapping launchfile
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([utbots_nav_launch_file_dir, '/utbots_navigation.launch.py']),
+            launch_arguments={'map': map_dir}.items()  # Pass map as an argument to the mapping launchfile
         ),
+        
         # Laser Filter
         Node(
             package='laser_filters',
@@ -76,6 +102,7 @@ def generate_launch_description():
                 ('scan_filtered', output_scan)
             ]
         ),
+        
         # Kalman Filter for IMU integration to Odom
         Node(
             package='robot_localization',
@@ -89,4 +116,5 @@ def generate_launch_description():
                 ])
             ]
         ),
+        
         ])
