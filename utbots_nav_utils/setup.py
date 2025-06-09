90d8e40ebab8c655e69586e3e59858aff4573727
ehg2004 <enzoholzmanngaio@alunos.utfpr.edu.br>
2025-06-09 20:23:30 -0300

    Added save_waypoint_service (need to be tested)

---
+++ utbots_nav_utils/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'utbots_nav_utils'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ehg2004',
+    maintainer_email='enzoholzmanngaio@alunos.utfpr.edu.br',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    # tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'save_waypoint_service = utbots_nav_utils.save_waypoint_service:main'
+        ],
+    },
+    extras_require={
+    'test': ['pytest', 'other-test-deps'],
+    },
+
+)
---
+++ utbots_nav_utils/utbots_nav_utils/save_waypoint_service.py
@@ -0,0 +1,92 @@
+# save_waypoint_service.py
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from utbots_srvs.srv import SaveWaypoint  # Update with your actual package name
+
+import yaml
+import os
+import subprocess
+
+import ament_index_python
+ament_index_python.get_package_share_directory
+class WaypointSaver(Node):
+    def __init__(self):
+        super().__init__('waypoint_saver')
+        self.current_pose = None
+        self.pose_sub = self.create_subscription(
+            PoseWithCovarianceStamped,
+            '/amcl_pose',
+            self.pose_callback,
+            10
+        )
+        self.path=(ament_index_python.get_package_share_directory('utbots_nav').rsplit('install/')[0]+
+                   "src/utbots_nav/map/")
+        print(self.path)
+        # self.waypoint_file=(os.system("ros2 param get /map_server yaml_filename"))#.rsplit(".yaml")+"_waypoints.yaml"
+        map_file = subprocess.check_output(
+          ["ros2", "param", "get", "/map_server", "yaml_filename"],
+            universal_newlines=True
+            ).rsplit("String value is: ")[1]
+        self.waypoint_file =map_file.rsplit(".yaml")[0]+"_waypoints.yaml"
+        print(self.waypoint_file)
+        print(map_file)
+        self.srv = self.create_service(SaveWaypoint, 'save_waypoint', self.save_waypoint_callback)
+        self.get_logger().info('Waypoint saver service ready.')
+
+    def pose_callback(self, msg):
+        self.current_pose = msg.pose.pose
+
+    def save_waypoint_callback(self, request, response):
+        name = request.name
+
+        if self.current_pose is None:
+            response.success = False
+            response.message = 'No pose received yet.'
+            return response
+
+        pose_dict = {
+            'position': {
+                'x': self.current_pose.position.x,
+                'y': self.current_pose.position.y,
+                'z': self.current_pose.position.z
+            },
+            'orientation': {
+                'x': self.current_pose.orientation.x,
+                'y': self.current_pose.orientation.y,
+                'z': self.current_pose.orientation.z,
+                'w': self.current_pose.orientation.w
+            }
+        }
+
+        # Load or create the YAML file
+        if os.path.exists(self.waypoint_file):
+            with open(self.waypoint_file, 'r') as f:
+                try:
+                    data = yaml.safe_load(f) or {}
+                except yaml.YAMLError:
+                    data = {}
+        else:
+            data = {}
+
+        data.setdefault('waypoints', {})
+        data['waypoints'][name] = pose_dict
+    
+        with open(self.waypoint_file, 'w') as f:
+            yaml.dump(data, f)
+
+        self.get_logger().info(f"Saved waypoint '{name}'")
+        response.success = True
+        response.message = f"Waypoint '{name}' saved successfully."
+        return response
+
+def main():
+    rclpy.init()
+    node = WaypointSaver()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
