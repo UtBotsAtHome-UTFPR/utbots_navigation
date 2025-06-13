57348b2a7e8d16e6533ec265a31467fc64893892
ehg2004 <enzoholzmanngaio@alunos.utfpr.edu.br>
2025-06-13 11:41:12 -0300

    added hestia launch with waypoints

--- utbots_nav_utils/launch/wp_hestia.launch.py
+++ utbots_nav_utils/launch/wp_hestia.launch.py
@@ -43,44 +43,26 @@ def launch_rqt_tools(context, *args, **kwargs):
 #                 proc.kill()
 
 def launch_setup(context, *args, **kwargs):
-    def set_world_map(map_file):
-        world_tb3_nav = os.path.join(get_package_share_directory('turtlebot3_navigation2'),"map/") + "map"
-        os.system(f"find {map_file} || cp {world_tb3_nav}.yaml {map_file}")
-        # print(world_tb3_nav+".yaml")
-        # print(map_file)
-        os.system(f"find {map_file.rsplit('.yaml')[0]}.pgm || cp {world_tb3_nav}.pgm {map_file.rsplit('.yaml')[0]}.pgm \
-                   && cat {map_file} | grep 'image: world.pgm' || sed -i '1s|.*|image: world.pgm|' {map_file}")
-
-        # print(world_tb3_nav+".pgm")
-        # print(map_file.rsplit('.yaml')[0]+".pgm")
-        return ([])
-
     map_name = LaunchConfiguration('map_name').perform(context)
+    lidar_port = LaunchConfiguration('lidar_port',default="/dev/ttyUSB0").perform(context)
 
     map_file = os.path.join(
         get_package_share_directory('utbots_nav').rsplit('install/')[0],
         "src","utbots_navigation" ,"utbots_nav", "map" #src/utbots_navigation/utbots_nav/map/
     ) + "/" + map_name + ".yaml"
-    
-    if(map_name=="world"):
-        set_world_map(map_file)
 
-    launch_dir_tb3_gz = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'), 'launch')
+#    ros2 launch utbots_nav map.launch.py    ## Launch all mapping packages (Gmapping)
+    launch_dir_utbots_nav = os.path.join(
+        get_package_share_directory('utbots_nav'), 'launch')
     
-    launch_dir_tb3_nav= os.path.join(
-        get_package_share_directory('turtlebot3_navigation2'), 'launch')
-
     return ([
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(launch_dir_tb3_nav, 'navigation2.launch.py')),
-            launch_arguments={'map': map_file}.items()
+            PythonLaunchDescriptionSource(os.path.join(launch_dir_utbots_nav, 'nav.launch.py')),
+            launch_arguments={'map': map_file, 
+                              'use_imu': False,
+                              'lidar_port': lidar_port }.items()
         ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(launch_dir_tb3_gz, 'turtlebot3_world.launch.py')),
-            # launch_arguments={'map': 'true'}.items()
-        ),       
+    
 
             Node(
                 package='utbots_nav_utils',
@@ -94,6 +76,7 @@ def launch_setup(context, *args, **kwargs):
                 # ],
                 
             ),
+
         Node(
             package='utbots_nav_utils',
             executable='save_waypoint_service',
